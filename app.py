c801252f8c29b1209dbe584265d879c0771e90b1
SzymonW66 <szymekwajs@gmail.com>
2022-06-13 15:17:16 +0200

    Dodanie bazy danych

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, url_for, make_response
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import abort, redirect, url_for, make_response
+from flask import Flask, flash, abort
 from flask_mail import Mail, Message
+import sqlite3
+# from flask_dance.contrib.github import make_github_blueprint, github
+import secrets
+import os
+import requests
 
 app = Flask(__name__)
 mail = Mail(app)
 
+app.config['SECRET_KEY'] = '1234'
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'szymekwajs@gmail.com'
@@ -45,8 +55,103 @@ def sendmail():
     msg = Message('Hello', sender='yourId@gmail.com', recipients=['someone1@gmail.com'])
     msg.body = "Hello Flask message sent from Flask-Mail"
     mail.send(msg)
-    return "Sent"
     return render_template('send_mail.html')
+
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        name = request.form['name']
+        title = request.form['title']
+        content = request.form['content']
+
+        if not name:
+            flash('Name is required!')
+        elif not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (name, title, content) VALUES (?, ?, ?)',
+                         (name, title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('guestbook'))
+
+    return render_template('create.html')
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+
+@app.route('/<int:id>/edit/', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        name = request.form['name']
+        title = request.form['title']
+        content = request.form['content']
+
+        if not name:
+            flash('Name is required!')
+
+        elif not title:
+            flash('Title is required!')
+
+        elif not content:
+            flash('Content is required!')
+
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET name = ?, title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (name, title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('guestbook'))
+
+    return render_template('edit.html', post=post)
+
+
+@app.route('/<int:id>/delete/', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('guestbook'))
+
+
+@app.route('/guestbook', methods=('GET', 'POST'))
+def guestbook():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts ORDER BY id DESC').fetchall()
+    conn.close()
+    return render_template('guestbook.html', posts=posts)
+
+@app.route('/guestbook-admin', methods=('GET', 'POST'))
+def guestbookAdmin():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts ORDER BY id DESC').fetchall()
+    conn.close()
+    return render_template('guestbook-admin.html', posts=posts)
 
 @app.errorhandler(404)
 def not_found_error(error):
@@ -60,4 +165,3 @@ def error_not_found():
 
 if __name__ == '__main__':
     app.run(port=8080)
-#TODO- Zrobić kolejną galerie w oparciu o booststrap 3 rzędy 3 kolumny z zdjęciami

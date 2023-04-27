049835b818ac6e294cea0ed62e02a08241c1029c
SzymonW66 <szymekwajs@gmail.com>
2023-04-27 22:02:15 +0200

    Prometheus Added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import abort, redirect, url_for, make_response
+from prometheus_client import start_http_server, Counter
 from flask import Flask, flash, abort
 from flask_mail import Mail, Message
 import sqlite3
@@ -11,7 +12,10 @@ import os
 import requests
 
 app = Flask(__name__)
+start_http_server(8000)
 mail = Mail(app)
+
+HTTP_REQUESTS = Counter('http_requests_total', 'Total HTTP Requests')
 
 app.secret_key = secrets.token_hex(16)  # generujemy sekretny klucz aplikacji
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
@@ -34,41 +38,49 @@ app.config['MAIL_USE_SSL'] = True
 
 @app.route('/')
 def home():
+    HTTP_REQUESTS.inc()
     return render_template('index.html')
 
 
 @app.route('/index2')
 def index2():
+    HTTP_REQUESTS.inc()
     return render_template('index2.html')
 
 
 @app.route('/about')
 def about():
+    HTTP_REQUESTS.inc()
     return render_template('about.html')
 
 
 @app.route('/gallery')
 def gallery():
+    HTTP_REQUESTS.inc()
     return render_template('gallery.html')
 
 
 @app.route('/contact')
 def contact():
+    HTTP_REQUESTS.inc()
     return render_template('contact.html')
 
 
 @app.route('/rock')
 def rock():
+    HTTP_REQUESTS.inc()
     return render_template('rock.html')
 
 
 @app.route('/gallery2')
 def gallery2():
+    HTTP_REQUESTS.inc()
     return render_template('gallery2.html')
 
 
 @app.route('/sendmail')
 def sendmail():
+    HTTP_REQUESTS.inc()
     msg = Message('Hello', sender='yourId@gmail.com', recipients=['someone1@gmail.com'])
     msg.body = "Hello Flask message sent from Flask-Mail"
     mail.send(msg)
@@ -77,6 +89,7 @@ def sendmail():
 
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
+    HTTP_REQUESTS.inc()
     if request.method == 'POST':
         name = request.form['name']
         title = request.form['title']
@@ -117,6 +130,7 @@ def get_post(post_id):
 
 @app.route('/<int:id>/edit/', methods=('GET', 'POST'))
 def edit(id):
+    HTTP_REQUESTS.inc()
     post = get_post(id)
 
     if request.method == 'POST':
@@ -147,6 +161,7 @@ def edit(id):
 
 @app.route('/<int:id>/delete/', methods=('POST',))
 def delete(id):
+    HTTP_REQUESTS.inc()
     post = get_post(id)
     conn = get_db_connection()
     conn.execute('DELETE FROM posts WHERE id = ?', (id,))
@@ -158,6 +173,7 @@ def delete(id):
 
 @app.route('/guestbook', methods=('GET', 'POST'))
 def guestbook():
+    HTTP_REQUESTS.inc()
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts ORDER BY id DESC').fetchall()
     conn.close()
@@ -166,6 +182,7 @@ def guestbook():
 
 @app.route('/guestbook-admin', methods=('GET', 'POST'))
 def guestbookAdmin():
+    HTTP_REQUESTS.inc()
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts ORDER BY id DESC').fetchall()
     conn.close()
@@ -174,6 +191,7 @@ def guestbookAdmin():
 
 @app.route("/login")
 def github_login():
+    HTTP_REQUESTS.inc()
     if not github.authorized:
         return redirect(url_for('github.login'))
     else:
@@ -185,6 +203,7 @@ def github_login():
 
 
 def format_response(city):
+    HTTP_REQUESTS.inc()
     weather_key = "6a2d34ead1a8f0627c3933eb6dd85d07"  # tu wklej swój KLUCZ API
     url = "https://api.openweathermap.org/data/2.5/weather"
     params = {"APPID": weather_key, "q": city, "units": "Metric"}
@@ -203,6 +222,7 @@ def format_response(city):
 
 @app.route('/weather', methods=['POST', 'GET'])
 def weather():
+    HTTP_REQUESTS.inc()
     if request.method == 'GET':
         return render_template('weather.html')
     if request.method == 'POST':
@@ -214,15 +234,17 @@ def weather():
 
 @app.errorhandler(404)
 def not_found_error(error):
+    HTTP_REQUESTS.inc()
     return render_template('404.html'), 404
 
 
 @app.route('/error_not_found')
 def error_not_found():
+    HTTP_REQUESTS.inc()
     response = make_response(render_template('404.html', name='ERROR 404'), 404)
     response.headers['X-Something'] = 'A value'
     return response
 
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run()
